9fda54e76f55fd4fed4ff916603db0941e412947
Ethan Holz <ethan2@vt.edu>
2020-12-03 10:25:26 -0500

    Added command line options

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # EasyCollegeCourses
 A Python Script For Picking The Easiest College Courses
+
+##HOW-TO
+To run interactively use the following:
+    python3 courseFinder.py
+To run from the command line:
+    python3 coursefinder.py -f exampleCourses.txt -id virginia-tech-vt
+This code can be run using a mix of arguments such as the following:
+    python3 coursefinder.py -f exampleCourses.txt
+    python3 coursefinder.py -id virginia-tech-vt
--- coursefinder.py
+++ coursefinder.py
@@ -2,7 +2,9 @@ import requests
 import json
 from time import sleep
 import csv
-
+import argparse
+import os
+import sys
 #Get Average GPA For Given Course
 def get_avg_gpa(university, subject, course):
 	resp = requests.get("https://anaanu.com/api/v1/course?university=" + university + "&subject=" + subject + "&course=" + course)
@@ -13,31 +15,40 @@ def get_avg_gpa(university, subject, course):
 		gpa = "NULL"
 	return gpa
 
-# Get Input For University / Courses
-school_name = input("Enter your University Anaanu ID:\n")
-file_or_paste = ""
-while file_or_paste != 'f' and file_or_paste != 'p':
-	file_or_paste = input("Would you like to input from a file or would you like to paste course names? (f/p)\n")
+def main():
+    #Parses argument files 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', action='store', 
+            help='Path to courses file')
+    parser.add_argument('-id',action='store',help='Anaanu ID')
+    args = parser.parse_args()
+    if args.id is not None:
+        school_name = str(args.id)
+    else:
+        school_name = input("Enter your University Anaanu ID:\n")
+    if args.file is not None:
+        file_name = str(args.file)
+        if not os.path.isdir(file_name):
+            print('The path specified does not exist')
+            sys.exit()
+        course_data = open(file_name, "r")
+    else:
+        course_data = input("Past course data \n")
+    # Request For Each Course
+    titles = course_data.read().split(",")
+    grades = []
+    for title in titles:
+            subject, course = title.split(" ")
+            print("Subject: " + subject + ", Course: " + course)
+            gpa = get_avg_gpa(school_name, subject, course)
+            print("GPA: ")
+            print(gpa)
+            grades.append(gpa)
 
-if file_or_paste == 'f':
-	file_name = input("File Name: \n")
-	courseData = open(file_name, "r")
-else:
-	courseData = input("Paste Course Data \n")
+    # Write to File
+    with open('results.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(zip(titles, grades))
 
-# Request For Each Course
-titles = courseData.read().split(",")
-grades = []
-for title in titles:
-	subject, course = title.split(" ")
-	print("Subject: " + subject + ", Course: " + course)
-	gpa = get_avg_gpa(school_name, subject, course)
-	print("GPA: ")
-	print(gpa)
-	grades.append(gpa)
-
-# Write to File
-with open('results.csv', 'w') as f:
-	writer = csv.writer(f)
-	writer.writerows(zip(titles, grades))
-
+if __name__ == "__main__":
+    main()
